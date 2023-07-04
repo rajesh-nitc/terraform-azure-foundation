f605356dd04cda3c7090937c28d89754b2071c70
rajesh-nitc <rajesh.nitc@gmail.com>
2023-07-04 16:22:20 +0530

    test gh workflow

--- 05-aca-infra/envs/dev/backend.tf
+++ 05-aca-infra/envs/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
     use_oidc             = true
-    storage_account_name = "storgtfstate"
-    container_name       = "stct-org-tfstate"
+    storage_account_name = "stbu1app1devwestustf"
+    container_name       = "stct-bu1-app1-dev-westus-tf"
     key                  = "dev-bu1-app1-aca-infra.tfstate"
-    resource_group_name  = "rg-org-tfstate"
+    resource_group_name  = "rg-bu1-app1-dev-westus-tf"
   }
 }
