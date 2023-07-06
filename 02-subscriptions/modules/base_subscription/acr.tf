58eff736c126e385af14529713f23209be5a6194
rajesh-nitc <rajesh.nitc@gmail.com>
2023-07-06 20:28:04 +0530

    test gh app workflow

--- 02-subscriptions/modules/base_subscription/acr.tf
+++ 02-subscriptions/modules/base_subscription/acr.tf
@@ -4,7 +4,7 @@ resource "azurerm_container_registry" "acr" {
   location                      = var.location
   sku                           = "Basic"
   admin_enabled                 = false
-  public_network_access_enabled = true # To push image from github
+  public_network_access_enabled = true # To push image from github action
   network_rule_bypass_option    = "AzureServices"
   network_rule_set              = []
 
