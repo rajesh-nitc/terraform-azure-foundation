33476a3b66f1b79c887ab1fac02cf09f6835abdc
rajesh-nitc <rajesh.nitc@gmail.com>
2023-07-06 20:14:39 +0530

    test gh app workflow

--- 02-subscriptions/modules/base_subscription/acr.tf
+++ 02-subscriptions/modules/base_subscription/acr.tf
@@ -1,13 +1,11 @@
-# resource "azurerm_container_registry" "acr" {
-#   name                = module.naming.container_registry.name
-#   resource_group_name = local.rg_name
-#   location            = var.location
+resource "azurerm_container_registry" "acr" {
+  name                          = module.naming.container_registry.name
+  resource_group_name           = local.rg_name
+  location                      = var.location
+  sku                           = "Premium"
+  admin_enabled                 = false
+  public_network_access_enabled = false
+  network_rule_bypass_option    = "AzureServices"
 
-#   sku = "Basic" #"Premium"
-
-#   admin_enabled                 = false
-#   public_network_access_enabled = true # Can we push image from github if it is false?
-#   network_rule_bypass_option    = "AzureServices"
-
-# }
+}
 
