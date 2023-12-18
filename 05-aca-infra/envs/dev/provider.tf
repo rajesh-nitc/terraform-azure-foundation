04b54da1cd845d0fe6aefb2cef0c808de2ae9c18
rajesh-nitc <rajesh.nitc@gmail.com>
2023-12-18 18:10:28 +0530

    test apim

--- 05-aca-infra/envs/dev/provider.tf
+++ 05-aca-infra/envs/dev/provider.tf
@@ -6,6 +6,14 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.61.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">=2.39.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = ">= 5.0"
+    }
   }
 }
 
