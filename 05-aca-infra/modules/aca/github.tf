358df89073fda4ef9390c6608ee8cee1aad01511
rajesh-nitc <rajesh.nitc@gmail.com>
2023-12-19 00:54:19 +0530

    default github token for workflow

--- 05-aca-infra/modules/aca/github.tf
+++ 05-aca-infra/modules/aca/github.tf
@@ -1,6 +1,7 @@
-# resource "github_actions_environment_secret" "apim_key" {
-#   repository      = split("/", var.repo)[1]
-#   environment     = var.env
-#   secret_name     = "REACT_APP_APIM_KEY"
-#   plaintext_value = random_password.apim.result
-# }
+# manually update the default permissions granted to the GITHUB_TOKEN to have read and write permissions in the repository for all scopes
+resource "github_actions_environment_secret" "apim_key" {
+  repository      = split("/", var.repo)[1]
+  environment     = var.env
+  secret_name     = "REACT_APP_APIM_KEY"
+  plaintext_value = random_password.apim.result
+}
