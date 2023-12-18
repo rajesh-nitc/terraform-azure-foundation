ceec482a64b0c2b318925aef7ff4d4ead69278a6
rajesh-nitc <rajesh.nitc@gmail.com>
2023-12-18 20:48:24 +0530

    test apim

--- 05-aca-infra/modules/aca/apim.tf
+++ 05-aca-infra/modules/aca/apim.tf
@@ -21,12 +21,14 @@ resource "azurerm_api_management" "apim" {
 
 }
 
+# apim subscriction key
 resource "random_password" "apim" {
   length  = 32
   special = false
 }
 
-resource "azurerm_api_management_subscription" "subsciption" {
+# apim subscriction key with scope: all apis
+resource "azurerm_api_management_subscription" "scope_all_apis" {
   api_management_name = azurerm_api_management.apim.name
   resource_group_name = local.rg_name
   primary_key         = random_password.apim.result
@@ -44,24 +46,41 @@ resource "azurerm_api_management_api" "api" {
   protocols           = ["https"]
 }
 
-resource "azurerm_api_management_api_policy" "policy" {
-  api_name            = azurerm_api_management_api.api.name
-  api_management_name = azurerm_api_management_api.api.api_management_name
-  resource_group_name = azurerm_api_management_api.api.resource_group_name
-
-  xml_content = <<XML
-<policies>
-  <inbound>
-    <validate-azure-ad-token tenant-id="${data.azurerm_client_config.current.tenant_id}">
-      <client-application-ids>
-          <application-id>${data.azurerm_user_assigned_identity.web.client_id}</application-id>
-      </client-application-ids>
-    </validate-azure-ad-token>
-    <authentication-managed-identity resource="${data.azuread_application.api.client_id}" client-id="${data.azurerm_user_assigned_identity.apim.client_id}" output-token-variable-name="msi-access-token" ignore-error="false"/>
-    <set-header name="Authorization" exists-action="override">
-      <value>@("Bearer " + (string)context.Variables["msi-access-token"])</value>
-    </set-header>
-  </inbound>
-</policies>
-XML
+resource "azurerm_api_management_product" "product" {
+  product_id            = "api"
+  api_management_name   = azurerm_api_management.apim.name
+  resource_group_name   = local.rg_name
+  display_name          = "API Product"
+  subscription_required = true
+  approval_required     = false
+  published             = true
 }
+
+resource "azurerm_api_management_product_api" "product_api" {
+  api_name            = azurerm_api_management_api.api.name
+  product_id          = azurerm_api_management_product.product.product_id
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = local.rg_name
+}
+
+# resource "azurerm_api_management_api_policy" "policy" {
+#   api_name            = azurerm_api_management_api.api.name
+#   api_management_name = azurerm_api_management_api.api.api_management_name
+#   resource_group_name = azurerm_api_management_api.api.resource_group_name
+
+#   xml_content = <<XML
+# <policies>
+#   <inbound>
+#     <validate-azure-ad-token tenant-id="${data.azurerm_client_config.current.tenant_id}">
+#       <client-application-ids>
+#           <application-id>${data.azurerm_user_assigned_identity.web.client_id}</application-id>
+#       </client-application-ids>
+#     </validate-azure-ad-token>
+#     <authentication-managed-identity resource="${data.azuread_application.api.client_id}" client-id="${data.azurerm_user_assigned_identity.apim.client_id}" output-token-variable-name="msi-access-token" ignore-error="false"/>
+#     <set-header name="Authorization" exists-action="override">
+#       <value>@("Bearer " + (string)context.Variables["msi-access-token"])</value>
+#     </set-header>
+#   </inbound>
+# </policies>
+# XML
+# }
